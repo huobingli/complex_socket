3de7609288b56e9f3bf17f58fd04b367326f6d0d
huobingli <huobingli@outlook.com>
2016-12-01 18:51:04 +0800

    lab update

---
+++ common/cacheTable.cpp
@@ -0,0 +1,30 @@
+
+//********************************************************
+//
+//       FileName:      cacheTable.cpp
+//
+//       Author:  huobingli - huobingli@outlook.com
+//       Description:    
+//       Create: 2016-12-01  18:47:51
+//       Last Modified: 2016-12-01 18:47:51
+//********************************************************
+// 
+#include <iostream>
+using namespace std;
+
+
+#ifndef _CACHETABLE_CPP
+#define _CACHETABLE_CPP
+class cacheNode {
+private:
+	
+
+};
+
+
+class cacheTable{
+
+
+};
+
+#endif
---
+++ common/socket.cpp
@@ -0,0 +1,22 @@
+
+//********************************************************
+//
+//       FileName:      socket.cpp
+//
+//       Author:  huobingli - huobingli@outlook.com
+//       Description:    
+//       Create: 2016-12-01  17:19:00
+//       Last Modified: 2016-12-01 17:19:00
+//********************************************************
+// 
+#include <iostream>
+
+#ifndef _SOCKET_CPP
+#define _SOCKET_CPP
+
+class socket{
+
+
+};
+
+#endif 
