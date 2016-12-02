c6c94c1f59fd7ce9d5de31dd05900e72aa103968
huobingli <huobingli@outlook.com>
2016-12-02 12:58:53 +0800

    lab commit

---
+++ client/clientSocket.cpp
@@ -0,0 +1,27 @@
+
+//********************************************************
+//
+//       FileName:      clientSocket.cpp
+//
+//       Author:  huobingli - huobingli@outlook.com
+//       Description:    
+//       Create: 2016-12-02  12:49:32
+//       Last Modified: 2016-12-02 12:49:32
+//********************************************************
+// 
+#include <iostream>
+
+#include "../common/socket.cpp"
+
+#ifndef _CLIENTSOCKET_CPP
+#define _CLIENTSOCKET_CPP
+class clientSocket : public socket {
+pirvate:
+	bool bConnected;
+
+public:
+	
+};
+
+
+#endif
